3c6f6addba65f2e61617658d6389fd115d9ee679
SvetlanaZhak <lana.zhak@gmail.com>
2020-03-07 14:49:58 +0200

    fix styles

--- Exercise3/App.js
+++ Exercise3/App.js
@@ -73,7 +73,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: "50%",
     marginTop: "50%"
   },
   rowButton: {
